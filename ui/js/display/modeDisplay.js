7c8766accec7150ddccb19f115fb23c12bf09e69
DEESOL <ducson9112k@gmail.com>
2025-03-29 18:59:45 +0700

    Update

--- ui/js/display/modeDisplay.js
+++ ui/js/display/modeDisplay.js
@@ -1,6 +1,17 @@
 export function initModeDisplay(mode) {
-    const modeDisplay = document.getElementById('mode-display');
-    if (modeDisplay && mode) {
-        modeDisplay.textContent = mode;
+    const ecoModeDisplay = document.getElementsByClassName('mode-eco-indicator')[0];
+    const sportModeDisplay = document.getElementsByClassName('mode-sport-indicator')[0];
+    if (ecoModeDisplay && sportModeDisplay) {
+        if (mode === 'eco') {
+            ecoModeDisplay.classList.add('active');
+        } else {
+            ecoModeDisplay.classList.remove('active');
+        }
+        
+        if (mode === 'sport') {
+            sportModeDisplay.classList.add('active'); 
+        } else {
+            sportModeDisplay.classList.remove('active'); 
+        }
     }
 }
